f211185e6b1ddc82c7ae4780a212a96bbae3b0bb
JungjoongKim <mpfreedom@nate.com>
2019-06-20 13:11:08 +0900

    gggh

---
+++ src/test/java/com/skyware/droidbluetooth/service/BluetoothServiceTest.java
@@ -0,0 +1,49 @@
+package com.skyware.droidbluetooth.service;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BluetoothServiceTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getDeviceFromBondedList() {
+    }
+
+    @Test
+    public void getDeviceState() {
+    }
+
+    @Test
+    public void enableBluetooth() {
+    }
+
+    @Test
+    public void selectDevice() {
+    }
+
+    @Test
+    public void connectToSelectedDevice() {
+    }
+
+    @Test
+    public void beginListenForData() {
+    }
+
+    @Test
+    public void finishListenForData() {
+    }
+}
