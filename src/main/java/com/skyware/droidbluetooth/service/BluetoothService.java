59f7d4d252e7172cfd058d3a05c76fc7fa9dec76
김정중 <jjkim@skyware.co.kr>
2019-11-12 11:00:04 +0900

    'init'

--- src/main/java/com/skyware/droidbluetooth/service/BluetoothService.java
+++ src/main/java/com/skyware/droidbluetooth/service/BluetoothService.java
@@ -33,7 +33,6 @@ public class BluetoothService {
     int mPariedDeviceCount = 0;
     Set<BluetoothDevice> mDevices;
 
-    // 스마트폰과 페어링 된 디바이스간 통신 채널에 대응 하는 BluetoothSocket
     BluetoothSocket mSocket = null;
     OutputStream mOutputStream = null;
     InputStream mInputStream = null;
@@ -57,17 +56,16 @@ public class BluetoothService {
         }
     }
 
-    // 블루투스 장치의 이름이 주어졌을때 해당 블루투스 장치 객체를 페어링 된 장치 목록에서 찾아내는 코드.
     BluetoothDevice getDeviceFromBondedList(String name) {
-        // BluetoothDevice : 페어링 된 기기 목록을 얻어옴.
         BluetoothDevice selectedDevice = null;
-        // getBondedDevices 함수가 반환하는 페어링 된 기기 목록은 Set 형식이며,
-        // Set 형식에서는 n 번째 원소를 얻어오는 방법이 없으므로 주어진 이름과 비교해서 찾는다.
+
         for(BluetoothDevice deivce : mDevices) {
-            // getName() : 단말기의 Bluetooth Adapter 이름을 반환
-            if(name.equals(deivce.getName())) {
-                selectedDevice = deivce;
-                break;
+            // Device Bluetooth Adapter Name return
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+                if(name.equals(deivce.getName())) {
+                    selectedDevice = deivce;
+                    break;
+                }
             }
         }
         return selectedDevice;
@@ -98,32 +96,29 @@ public class BluetoothService {
         }
     }
 
-    // 블루투스 지원하며 활성 상태인 경우.
     public void selectDevice(final Context context) {
-        // 블루투스 디바이스는 연결해서 사용하기 전에 먼저 페어링 되어야만 한다
-        // getBondedDevices() : 페어링된 장치 목록 얻어오는 함수.
-        mDevices = btAdapter.getBondedDevices();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            mDevices = btAdapter.getBondedDevices();
+        }
         mPariedDeviceCount = mDevices.size();
 
         if(mPariedDeviceCount == 0 ) { // 페어링된 장치가 없는 경우.
             Toast.makeText(context.getApplicationContext(), "페어링된 장치가 없습니다.", Toast.LENGTH_LONG).show();
             //finish(); // App 종료.
         }
-        // 페어링된 장치가 있는 경우.
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("블루투스 장치 선택");
 
         // 각 디바이스는 이름과(서로 다른) 주소를 가진다. 페어링 된 디바이스들을 표시한다.
         List<String> listItems = new ArrayList<String>();
         for(BluetoothDevice device : mDevices) {
-            // device.getName() : 단말기의 Bluetooth Adapter 이름을 반환.
-            listItems.add(device.getName());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+                listItems.add(device.getName());
+            }
         }
         listItems.add("취소");  // 취소 항목 추가.
 
-
-        // CharSequence : 변경 가능한 문자열.
-        // toArray : List형태로 넘어온것 배열로 바꿔서 처리하기 위한 toArray() 함수.
         final CharSequence[] items = listItems.toArray(new CharSequence[listItems.size()]);
         // toArray 함수를 이용해서 size만큼 배열이 생성 되었다.
         listItems.toArray(new CharSequence[listItems.size()]);
@@ -134,7 +129,9 @@ public class BluetoothService {
             public void onClick(DialogInterface dialog, int item) {
                 // TODO Auto-generated method stub
                 if(item == mPariedDeviceCount) { // 연결할 장치를 선택하지 않고 '취소' 를 누른 경우.
-                    Toast.makeText(builder.getContext().getApplicationContext(), "연결할 장치를 선택하지 않았습니다.", Toast.LENGTH_LONG).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        Toast.makeText(builder.getContext().getApplicationContext(), "연결할 장치를 선택하지 않았습니다.", Toast.LENGTH_LONG).show();
+                    }
                     //finish();
                 }
                 else { // 연결할 장치를 선택한 경우, 선택한 장치와 연결을 시도함.
@@ -150,31 +147,20 @@ public class BluetoothService {
     }
 
 
-    //  connectToSelectedDevice() : 원격 장치와 연결하는 과정을 나타냄.
-    //   실제 데이터 송수신을 위해서는 소켓으로부터 입출력 스트림을 얻고 입출력 스트림을 이용하여 이루어 진다.
     public void connectToSelectedDevice(String selectedDeviceName, Context context) {
-        // BluetoothDevice 원격 블루투스 기기를 나타냄.
         mRemoteDevie = getDeviceFromBondedList(selectedDeviceName);
-        // java.util.UUID.fromString : 자바에서 중복되지 않는 Unique 키 생성.
-        //UUID uuid = java.util.UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-        UUID uuid = java.util.UUID.fromString(UUID.randomUUID().toString());
+        // 자바에서 중복되지 않는 Unique 키 생성.
+        UUID uuid = java.util.UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
         try {
-            // 소켓 생성, RFCOMM 채널을 통한 연결.
-            // createRfcommSocketToServiceRecord(uuid) : 이 함수를 사용하여 원격 블루투스 장치와
-            //                                           통신할 수 있는 소켓을 생성함.
-            // 이 메소드가 성공하면 스마트폰과 페어링 된 디바이스간 통신 채널에 대응하는
-            //  BluetoothSocket 오브젝트를 리턴함.
-            mSocket = mRemoteDevie.createRfcommSocketToServiceRecord(uuid);
-            mSocket.connect(); // 소켓이 생성 되면 connect() 함수를 호출함으로써 두기기의 연결은 완료된다.
 
-            // 데이터 송수신을 위한 스트림 얻기.
-            // BluetoothSocket 오브젝트는 두개의 Stream을 제공한다.
-            // 1. 데이터를 보내기 위한 OutputStrem
-            // 2. 데이터를 받기 위한 InputStream
-            mOutputStream = mSocket.getOutputStream();
-            mInputStream = mSocket.getInputStream();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+                mSocket = mRemoteDevie.createRfcommSocketToServiceRecord(uuid);
+                mSocket.connect();
 
+                mOutputStream = mSocket.getOutputStream();
+                mInputStream = mSocket.getInputStream();
+            }
             // 데이터 수신 준비.
             beginListenForData(context);
 
@@ -198,9 +184,6 @@ public class BluetoothService {
         {
             @Override
             public void run() {
-                // interrupt() 메소드를 이용 스레드를 종료시키는 예제이다.
-                // interrupt() 메소드는 하던 일을 멈추는 메소드이다.
-                // isInterrupted() 메소드를 사용하여 멈추었을 경우 반복문을 나가서 스레드가 종료하게 된다.
                 while(!Thread.currentThread().isInterrupted()) {
                     try {
                         // InputStream.available() : 다른 스레드에서 blocking 하기 전까지 읽은 수 있는 문자열 개수를 반환함.
@@ -213,8 +196,7 @@ public class BluetoothService {
                                 byte b = packetBytes[i];
                                 if(b == mCharDelimiter) {
                                     byte[] encodedBytes = new byte[readBufferPosition];
-                                    //  System.arraycopy(복사할 배열, 복사시작점, 복사된 배열, 붙이기 시작점, 복사할 개수)
-                                    //  readBuffer 배열을 처음 부터 끝까지 encodedBytes 배열로 복사.
+
                                     System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
 
                                     final String data = new String(encodedBytes, "US-ASCII");
